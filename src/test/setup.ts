b8251a121065daa92cd87da5d21c4d136e7a28ca
shokhruzisroilov <shokhruzisroilov@gmail.com>
2025-11-28 16:20:56 +0500

    clear

--- src/test/setup.ts
+++
@@ -1,9 +0,0 @@
-import { expect, afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/vitest';
-import 'fake-indexeddb/auto';
-
-// Cleanup after each test
-afterEach(() => {
-  cleanup();
-});
